3b04c730fafa0fdff83d21d8a0c314e7628646cd
marveltracker <marveltracker@marvellabs.com>
2024-10-14 19:14:39 +1100

    chore: fix clerk webhooks routing

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,11 @@ import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/api/webhooks(.*)",
+  "/api/webhooks/clerk(.*)",
 ]);
 
 export default clerkMiddleware((auth, request) => {
+  console.log("Executing clerk middleware");
   if (!isPublicRoute(request)) {
     auth().protect();
   }
